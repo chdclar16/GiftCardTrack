5d1227c49c3cb77f625a3c9426dc1796bbba50b7
Chad Manuel <chadc_manuel@yahoo.com>
2023-09-27 14:59:37 -0700

    Added tailwind to project and added nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,25 @@
 import { Link } from 'react-router-dom';
+import {IoHomeOutline} from 'react-icons/io5'
 
 const NavBar = () => {
     return (
-        <nav>
-            <Link to='/'>Home</Link>
-        </nav>
+        <div className='bg-blue-500'>
+            <nav className='relative px-4 py-4 flex justify-between items-center'>
+                <Link className='text-3xl font-bold leading-none w-8 h-8'>
+                    <img src="/images/giftCard.png" alt="Gift Card" className='w-8 h-8' />
+                </Link>
+                <Link to='/' className='hover:scale-150 duration-150 ease-in-out'>
+                    <IoHomeOutline />
+                </Link>
+                <Link>
+                    <span>Cards</span>
+                </Link>
+                <Link>
+                    <button>Login</button>
+                </Link>
+            </nav>
+        </div>
     )
 }
+
+export default NavBar
